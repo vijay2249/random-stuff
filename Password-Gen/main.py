74f1c7195e1e3ca62feaac758f57f736a4830da1
Vijay HCN <vijaychowdary3699@gmail.com>
2023-09-09 14:35:38 +0530

    gen-password py script

---
+++ Password-Gen/main.py
@@ -0,0 +1,28 @@
+import string
+import pyperclip
+import secrets
+import logging
+import argparse
+
+WARN = "[=]"
+INFO = "[+]"
+ERROR = "[-]"
+
+
+def get_arguments():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-l", "--length", dest="Length", help="Length of password", type=int)
+    options = parser.parse_args()
+    if not options.Length:
+        logging.warning(f"{WARN} length of password argument not provided, considering default value of 12")
+        options.Length = 12
+    return options
+
+user_input = get_arguments()
+letters, digits, special_chars = string.ascii_letters, string.digits, string.punctuation
+chars = letters + digits + special_chars
+
+gen_password = "".join(secrets.choice(chars) for i in range(user_input.Length))
+print(f"[+] Generated password - {gen_password}")
+pyperclip.copy(gen_password)
+print(f"[+] Password copied to clipboard")
